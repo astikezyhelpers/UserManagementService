229ff1441f1627cb98f3dc7ec3d3faaf05a61cb3
Sasikiran Dommara <sasikirandommara404@gmail.com>
2025-07-22 17:27:40 +0530

    Verified all end points are working

--- messabebroker/consumer.js
+++ messabebroker/consumer.js
@@ -1,20 +1,20 @@
 import { connectRabbitMQ } from '../config/rabbitmq.js';
-import sendEmail from '../emailServices/email.js';
+import sendEmail from '../emailservice/email.js';
 
 const QUEUE_NAME = 'email_verification';
 
-/**
- * Start consuming email_verification queue and send emails
- */
 export const startConsumer = async () => {
+  console.log('Starting consumer...');
   const channel = await connectRabbitMQ();
   await channel.assertQueue(QUEUE_NAME, { durable: true });
   console.log(`Consumer listening on queue: ${QUEUE_NAME}`);
 
   channel.consume(QUEUE_NAME, async (msg) => {
+    console.log('Message received:', msg ? msg.content.toString() : 'null');
     if (msg) {
       const { email, token } = JSON.parse(msg.content.toString());
       try {
+        console.log('Sending email to:', email, 'with token:', token);
         await sendEmail(email, token);
         channel.ack(msg);
       } catch (err) {
@@ -24,4 +24,7 @@ export const startConsumer = async () => {
     }
   });
 };
+
+// Make sure to call startConsumer if this is your entry file:
+startConsumer();
 
